9dcec737854c56c2a8bee46cb2ce05d3d5f97761
Vinícius Barbosa <vinibar95@gmail.com>
2023-09-15 17:51:13 -0300

    Finalização de modelo estrutural

--- src/components/form/Buttton.js
+++ src/components/form/Buttton.js
@@ -1,9 +1,14 @@
 import styles from './Button.module.css'
 
 function Button({BtnText}){
+
+    function submit(e){
+        e.preventDefault()
+    }
+
     return(
         <>
-            <button className={styles.btn}>{BtnText}</button>
+            <button onClick={submit} className={styles.btn}>{BtnText}</button>
         </>
     )
 }
